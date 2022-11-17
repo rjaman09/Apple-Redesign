ec2be9d216261ffe3cac113fb2bc801e6218495d
admin <admin@MacBook-Air.local>
2022-11-17 16:23:42 +0530

    finally completd

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["rb.gy", "www.gravatar.com", "cdn.sanity.io", "lh3.googleusercontent.com"],
+  },
+};
